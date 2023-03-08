4a645d5c69da1b03720e3afae6018dc95247be67
yomexican <gyorimartin.mail@gmail.com>
2023-03-08 11:32:35 +0100

    A szög függvényelkészült

--- main1.py
+++ main1.py
@@ -9,6 +9,10 @@ def Haromszogterulete(aa, bb, cc):
     TT = math.sqrt( s(s-aa)*(s-bb)*(s-cc) )
     return TT
 
+def Szog(m1, m2, sz):     #szögmelleti1,szögmelleti2,szöggelszemben
+    ertek=(m1**2 + m2**2 - sz**2)/(2*m1*m2)
+    return math.degrees( math.acos(ertek))
+
 #0
 print("Háromszög feldolgozása függvények segítségével.")
 
@@ -22,7 +26,13 @@ c = float(reszletek[2].replace(',' , ';'))
 #2
 K = Haromszogkerulete(a, b, c)
 T = Haromszogterulete(a, b, c)
+alfa = Szog(b, c, a)
+betta = Szog(a, c, b)
+gamma = Szog(a, b, c)
 
 #3
 print(f"A(z) {a:.2f}cm és {b:.2f}cm és {c:.2f}cm oldal hosszúságú a háromszög kerülete: {K:.2}cm")
 print(f"A(z) {a:.2f}cm és {b:.2f}cm és {c:.2f}cm oldal hosszúságú a háromszög területe: {T:.2}cm")
+print(f"alfa: {alfa:.2f}°")
+print(f"betta: {betta:.2f}°")
+print(f"gamma: {gamma:.2f}°")
